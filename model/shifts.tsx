718d67d32b009c6a706b640156b3dc7ee5e206d0
Samer Alkhatib <sameralkhatib@Samers-MacBook-Pro.local>
2023-03-31 19:28:04 +0200

    remove column from shifts model

--- model/shifts.tsx
+++ model/shifts.tsx
@@ -1,12 +1,10 @@
 import mongoose, { Schema, model, models } from "mongoose";
-import Facilities from "./facilities";
 
 const shiftSchema = new Schema({
   name: String,
   from: String,
   to: String,
   quarter: String,
-  facility: Facilities.schema,
 });
 
 const Shifts = models.shift || model("shift", shiftSchema);
